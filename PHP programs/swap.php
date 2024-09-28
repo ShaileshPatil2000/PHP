5a535f6b1ccb6cc963f594b00eb8f36e2c8d5f95
ShaileshPatil2000 <92880083+ShaileshPatil2000@users.noreply.github.com>
2024-09-28 20:06:42 +0530

    Add files via upload

---
+++ PHP programs/swap.php
@@ -0,0 +1,17 @@
+<?php
+//Write a prom to swap two number without using third varible?
+$no1=20;
+$no2=30;
+echo"Before swapping</br>";
+echo "no1=$no1</br>";
+echo "no2=$no2</br>";
+
+$no1=$no1+$no2;
+$no2=$no1-$no2;
+$no1=$no1-$no2;
+
+echo"Before swapping</br>";
+echo "no1=$no1</br>";
+echo "no2=$no2</br>";
+
+?>

9bc524d97efcfe3c2bfb9bb7fb6ec985a4528b6d
ShaileshPatil2000 <92880083+ShaileshPatil2000@users.noreply.github.com>
2024-09-29 22:10:39 +0530

    Add files via upload

---
+++ PHP programs/Fcatorial_in_range.php
@@ -0,0 +1,21 @@
+<?php
+$temp=0;
+for($no=1;$no<=100;$no++)
+{
+    for($i=2;$i<=$no-1;$i++)
+    {
+        if($no%$i==0)
+        {
+            $temp=1;
+        }
+    }
+    if($temp==0)
+    {
+        echo "$no<br>";
+    }
+    else
+    {
+        $temp=0;
+    }
+}
+?>
